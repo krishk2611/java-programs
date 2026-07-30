46b2da72c26b235359ce658cd21dc36f6770c6a7
krishk2611 <krish.kapdi143646@marwadiuniversity.ac.in>
2026-07-30 12:12:29 +0530

    Add files via upload

---
+++ javach1/CommandAdd.java
@@ -0,0 +1,11 @@
+class CommandAdd
+{
+	public static void main(String args[])
+	{
+		int no1 = Integer.parseInt(args[0]);
+		int no2 = Integer.parseInt(args[1]);
+		
+		int Add = no1 + no2 ;
+		System.out.println("your total is " +Add);
+	}
+}
